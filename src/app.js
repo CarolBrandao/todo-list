952f060323679ea6089760eb4c79a464969e6eab
carolbrandao <bcarol744@gmail.com>
2020-01-03 11:09:03 +0100

    Add reducers and actions

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo.svg" className="App-logo" alt="logo" />
+        <p>
+          This is a todo list application.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
